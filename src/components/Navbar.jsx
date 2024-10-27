4d42764046de72c1c880829d44f9e9af90bebf3d
Rahul Gawade <rahulgawade360@gmail.com>
2024-10-28 01:42:06 +0530

    Fix import issue for Aboutus component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Homepage from './Homepage';
-import Login from './Login';
-import AboutUs from './Aboutus';
 
 const Navbar = () => {
   return (
